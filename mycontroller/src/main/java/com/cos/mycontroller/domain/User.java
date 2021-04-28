9f2c995f18daceaba065bbf4764f9ac7e8ef0917
LeeBG <donny1848@gmail.com>
2021-04-28 15:28:46 +0900

    controller 맛보기-(2)

--- mycontroller/src/main/java/com/cos/mycontroller/domain/User.java
+++ mycontroller/src/main/java/com/cos/mycontroller/domain/User.java
@@ -8,7 +8,22 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+	//1. username=ssar&password=1234 = > 필드있네???
 	private String username;
 	private String password;
-	private String phone;
+	
+	public String getUsername() {
+		return username;
+	}
+	//2.setter호출
+	public void setUsername(String username) {
+		this.username=username; 
+	}
+	public String getPassword() {
+		return password;
+	}
+	//2.setter호출
+	public void setPassword(String password) {
+		this.password=password;
+	}
 }
